dd189aee23d06e81ed49ad67db72cdd63a67c099
Harry de Assis Guimarães <harryguimaraes@mtprev.local>
2025-03-24 13:55:25 -0400

    TodoList

---
+++ script.js
@@ -0,0 +1,11 @@
+function add() {
+    let content = document.getElementById('content').innerText;
+    let notes = document.getElementById('notes');
+
+    if (content == '') {
+        window.alert("Digite algo")
+    }
+    else {
+        
+    }
+}
